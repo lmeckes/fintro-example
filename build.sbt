fedda535988fb3fc8b5bcdb9b905e7d82236ba3e
louis.meckes@springernature.com <louis.meckes@springernature.com>
2017-01-03 09:04:02 +0000

    init rep

---
+++ build.sbt
@@ -0,0 +1,3 @@
+resolvers += "JCenter" at "https://jcenter.bintray.com"
+libraryDependencies += "io.fintrospect" % "fintrospect-core_2.11" % "13.14.0"
+libraryDependencies += "com.github.spullara.mustache.java" % "compiler" % "0.9.4"
---
+++ src/main/scala/FormValidationApp.scala
@@ -0,0 +1,32 @@
+package examples.formvalidation
+
+import java.net.URL
+
+import com.twitter.finagle.Http
+import com.twitter.finagle.http.filter.Cors
+import com.twitter.finagle.http.filter.Cors.HttpFilter
+import com.twitter.finagle.http.path.Root
+import com.twitter.util.Await
+import io.fintrospect.ModuleSpec
+import io.fintrospect.formats.Html
+import io.fintrospect.renderers.SiteMapModuleRenderer
+import io.fintrospect.templating.{MustacheTemplates, RenderView}
+
+/**
+  * This example shows how to use Body.webform() and a templating engine to construct a validating form, with custom messages
+  * for each field.
+  */
+object FormValidationApp extends App {
+
+  val renderView = new RenderView(Html.ResponseBuilder, MustacheTemplates.HotReload("src/main/resources"))
+
+  val module = ModuleSpec(Root, new SiteMapModuleRenderer(new URL("http://my.cool.app")), renderView)
+    .withDescriptionPath(_ / "sitemap.xml")
+    .withRoutes(new ReportAge(new GreetingDatabase))
+
+  println("See the validating form at: http://localhost:8181")
+
+  Await.ready(
+    Http.serve(":8181", new HttpFilter(Cors.UnsafePermissivePolicy).andThen(module.toService))
+  )
+}
